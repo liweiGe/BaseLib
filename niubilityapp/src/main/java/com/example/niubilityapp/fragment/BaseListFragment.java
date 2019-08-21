d3a1957f1f1483edde6aa2106a13f73759966481
lwqiu <qlw2030@163.com>
2019-08-21 15:57:29 +0800

    升级studio

---
+++ niubilityapp/src/main/java/com/example/niubilityapp/fragment/BaseListFragment.java
@@ -0,0 +1,29 @@
+package com.example.niubilityapp.fragment;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.niubilityapp.R;
+import com.kongzue.baseframework.base.BaseFragment;
+import com.kongzue.baseframework.interfaces.Layout;
+
+@Layout(R.layout.activity_recycler_view)
+public abstract class BaseListFragment extends BaseFragment {
+
+    public RecyclerView recyclerView;
+
+
+
+    @Override
+    public void initViews() {
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+
+    }
+
+
+
+    @Override
+    public void setEvents() {
+
+    }
+
+}
--- niubilityapp/src/main/java/com/example/niubilityapp/fragment/CityTvFragment.java
+++ niubilityapp/src/main/java/com/example/niubilityapp/fragment/CityTvFragment.java
@@ -1,36 +1,21 @@
 package com.example.niubilityapp.fragment;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.niubilityapp.R;
 import com.example.niubilityapp.live_TV.LiveAdapter;
 import com.example.niubilityapp.live_TV.LiveUrl;
-import com.kongzue.baseframework.base.BaseFragment;
 import com.kongzue.baseframework.interfaces.Layout;
 
 @Layout(R.layout.activity_recycler_view)
-public class CityTvFragment extends BaseFragment {
-
-    private RecyclerView recyclerView;
+public class CityTvFragment extends BaseListFragment {
 
     public static Fragment newInstance() {
         return new CityTvFragment();
     }
 
     @Override
-    public void initViews() {
-        recyclerView = (RecyclerView) findViewById(R.id.rv);
-
-    }
-
-    @Override
     public void initDatas() {
         recyclerView.setAdapter(new LiveAdapter(LiveUrl.getCityVideoList(), me));
-    }
-
-    @Override
-    public void setEvents() {
-
     }
 }
