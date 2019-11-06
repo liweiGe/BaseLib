74724084ebdc371f6f38bda46b1641fb73ba185f
lwqiu <qlw2030@163.com>
2019-11-06 17:12:00 +0800

    base库添加悬浮吸顶(通过ItemDecoration 实现),使用靠谱

---
+++ baseframework/src/main/java/com/kongzue/baseframework/StickyRvHeader/listener/PowerGroupListener.java
@@ -0,0 +1,16 @@
+package com.kongzue.baseframework.StickyRvHeader.listener;
+
+import android.view.View;
+
+/**
+ * Created by gavin
+ * Created date 17/5/25
+ * 显示自定义View的Group监听
+ */
+
+public interface PowerGroupListener {
+
+    String getGroupName(int position);
+
+    View getGroupView(int position);
+}
--- baseframework/src/main/java/com/kongzue/baseframework/swipeback/util/SwipeBackActivityBase.java
+++ baseframework/src/main/java/com/kongzue/baseframework/swipeback/util/SwipeBackActivityBase.java
@@ -7,13 +7,13 @@ public interface SwipeBackActivityBase {
     /**
      * @return the SwipeBackLayout associated with this activity.
      */
-    public abstract SwipeBackLayout getSwipeBackLayout();
-    
-    public abstract void setSwipeBackEnable(boolean enable);
-    
+    SwipeBackLayout getSwipeBackLayout();
+
+    void setSwipeBackEnable(boolean enable);
+
     /**
      * Scroll out contentView and finish the activity
      */
-    public abstract void scrollToFinishActivity();
-    
+    void scrollToFinishActivity();
+
 }
